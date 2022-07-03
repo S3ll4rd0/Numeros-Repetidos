b5c8319d6c07f6a23db5315859a27d83cf334c33
Antonio Nicolás Salmerón Rubio <54323367+S3ll4rd0@users.noreply.github.com>
2022-07-03 22:43:24 +0200

    Add files via upload
    Añado los archivos del ejercicio realizado en clase - Ejecuta bien, error en salida, se repite el mensaje

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Repetidos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(337, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Introduce aquí";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(182, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Devolver Moda";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(271, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Introduce números separados por una coma";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(20, 249);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(450, 20);
+            this.label2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 353);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Repetidos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] listaNumString = textBox1.Text.Split(',');
+            int[] listaNumInt = new int[listaNumString.Length];
+
+            int max = 0;
+            for (int i = 0; i < listaNumInt.Length; i++)
+            {
+                listaNumInt[i] = int.Parse(listaNumString[i]);
+            }
+            int[] moda = new int[listaNumString.Length];
+            for (int i = 0; i < listaNumString.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < listaNumString.Length; j++)
+                {
+                    if (listaNumInt[i] == listaNumInt[j])
+                    {
+                        count++;
+                    }
+                    if (count > max)
+                    {
+                        max = count;
+                    }
+                }
+                moda[i] = count;
+            }
+            
+            int[] orden = new int[listaNumString.Length];
+            for (int i = 0; i < listaNumString.Length; i++)
+            {
+                if (moda[i] == max)
+                {
+                    MessageBox.Show(listaNumInt[i].ToString() + " -> " + moda[i]);
+                }
+            }
+        }
+    }
+}
